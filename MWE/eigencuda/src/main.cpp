13d2aef4c76578b76f108b60e1f45d3e0c0b48dc
Tanmay Bishnoi <tbishnoi@torontomu.ca>
2024-09-08 01:49:39 +0530

    finished vector dot example

--- MWE/eigencuda/src/main.cpp
+++ MWE/eigencuda/src/main.cpp
@@ -3,49 +3,34 @@
 #include <algorithm>
 #include <Eigen/Core>
 #include <chrono>
-#include <kernel.hpp>
+#include <dot.hpp>
 
-// extern std::vector<float> launch_vecadd(
-//   std::vector<Eigen::Vector3f>,
-//   std::vector<Eigen::Vector3f>
-// );
 
-// extern "C" std::vector<float> launch_vecadd(
-//   std::vector<Eigen::Vector3f> u, 
-//   std::vector<Eigen::Vector3f> v
-// );
-
-// std::vector<float> launch_vecadd(
-//   std::vector<Eigen::Vector3f> u, 
-//   std::vector<Eigen::Vector3f> v
-// );
 
 std::vector<std::vector<Eigen::Vector3f>> generate_vectors()
 {
   // ready the vectors
   // int len_vec = 134217728; // 2^27
-  int len_vec = 10; // 2^27
-  int seed = 0;
-  std::srand(unsigned(seed));
-  std::vector<Eigen::Vector3f> u(len_vec, Eigen::Vector3f {1.0,1.0,1.0});
-  // std::generate(u.begin(), u.end(), std::rand);
-  auto v = u;
-  return std::vector<std::vector<Eigen::Vector3f>> {u,v};
+  int len_vec = 4000;
+  // int len_vec = 256; 
+  std::vector<Eigen::Vector3f> v1(len_vec, Eigen::Vector3f {1.0,1.0,1.0});
+  auto v2 = v1;
+  return std::vector<std::vector<Eigen::Vector3f>> {v1,v2};
 }
 
 int main(){
   auto vectors = generate_vectors();
   std::cout<<"Code Runs! with Eigen!"<<std::endl;
 
-  // for(auto it=vectors.begin(); it!=vectors.end(); it++){
-  //     std::cout<<(*it)[0]<<" "<<(*it)[1]<<" "<<(*it)[2]<<std::endl;
-  // }
   auto start = std::chrono::high_resolution_clock::now();
-  // auto output = launch_vecadd(vectors[0], vectors[1]);
-  hello_world();
+  auto output = launch_vecadd(vectors[0], vectors[1]);
   auto end = std::chrono::high_resolution_clock::now();
   std::chrono::duration<double> duration = end - start;
   std::cout << "Function execution time: " << duration.count() << " seconds" << std::endl;    
+
+  for(int i=0; i<5; i++){
+    std::cout<<output[i]<<std::endl;
+  }
 
   return 0;
 }
